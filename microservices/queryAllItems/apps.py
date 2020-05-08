5e7f533287bf157ea5c606016ba2a905e57ef630
doom <Brandoningram1996@gmail.com>
2020-05-08 15:48:32 -0700

    Model for saving a set of orders

---
+++ microservices/queryAllItems/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class QueryallitemsConfig(AppConfig):
+    name = 'microservices.queryAllItems'
---
+++ microservices/queryAllItems/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+# Create your models here.
+class BazaarOrders(models.Model):
+    amount = models.IntegerField()
+    ordersNum = models.IntegerField()
+    pricePerUnit = models.FloatField()
+    sell_or_buy = models.BooleanField()
+    item_id = models.CharField(max_length=255)
+    item_name = models.CharField(max_length=255)
