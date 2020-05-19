8283bc05c21088b6f8a198512a1c56e15d116a96
doom <Brandoningram1996@gmail.com>
2020-05-19 12:28:01 -0700

    Added urls for std deviation info

--- microservices/SingleItem/views.py
+++ microservices/SingleItem/views.py
@@ -16,8 +16,26 @@ def index_buys_view(request, item) -> HttpResponse:
     weighted_avg = get_weighted_average(test)
     std_deviation(test, weighted_avg, item)
     std_deviation_object = std_deviation(test, weighted_avg, item)
-    print(std_deviation_object.lower_range, std_deviation_object.upper_range,' - ', weighted_avg)
+    print(item)
+    print('lower range:', std_deviation_object.lower_range, '\n', 'upper range:', std_deviation_object.upper_range, '\n', 'average price:', weighted_avg)
     return HttpResponse(json.dumps(test))
+
+
+def buys_index_std_deviation_info(request, item) -> HttpResponse:
+    json_loaded = get_data_from_external_api()
+    test = get_item_buys(item, 100, jsonLoaded=json_loaded)
+    weighted_avg = get_weighted_average(test)
+    std_deviation(test, weighted_avg, item)
+    std_deviation_object = std_deviation(test, weighted_avg, item)
+    return HttpResponse(json.dumps(('lower range:', std_deviation_object.lower_range, '\n', 'upper range:', std_deviation_object.upper_range, '\n', 'average price:', weighted_avg)))
+
+
+def sells_index_std_deviation_info(request, item) -> HttpResponse:
+    json_loaded = get_data_from_external_api()
+    test = get_item_sells(item, 100, jsonLoaded=json_loaded)
+    weighted_avg = get_weighted_average(test)
+    std_deviation_object = std_deviation(test, weighted_avg, item)
+    return HttpResponse(json.dumps(('lower range:', std_deviation_object.lower_range, '\n', 'upper range:', std_deviation_object.upper_range, '\n', 'average price:', weighted_avg)))
 
 
 def index_sells_view(request, item) -> HttpResponse:
@@ -25,7 +43,8 @@ def index_sells_view(request, item) -> HttpResponse:
     test = get_item_sells(item, 100, jsonLoaded=json_loaded)
     weighted_avg = get_weighted_average(test)
     std_deviation_object = std_deviation(test, weighted_avg, item)
-    print(std_deviation_object.lower_range, std_deviation_object.upper_range,' - ', weighted_avg)
+    print(item)
+    print('lower range:', std_deviation_object.lower_range, '\n', 'upper range:', std_deviation_object.upper_range, '\n', 'average price:', weighted_avg)
     return HttpResponse(json.dumps(test))
 
 
